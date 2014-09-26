1c0c4af4871b33515097ccaedb03e8c0364dfd99
John Wilson <johnleew@agraf.cs.washington.edu>
2014-09-26 12:08:05 -0700

    changes

--- src/main/java/com/cse490m/app/App.java
+++ src/main/java/com/cse490m/app/App.java
@@ -1,18 +1,27 @@
 package com.cse490m.app;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 
 public class App {
   
   public static void main(String[] args) throws Exception {
     Server server = new Server(8080);
     
-    ContextHandler context = new ContextHandler();
-    context.setContextPath("/");
-    context.setHandler(new HelloHandler());
-    server.setHandler(context);
+    ContextHandler helloContext = new ContextHandler();
+    helloContext.setContextPath("/hello");
+    helloContext.setHandler(new HelloHandler());
     
+    ContextHandler loginContext = new ContextHandler();
+    loginContext.setContextPath("/login");
+    loginContext.setHandler(new LoginHandler());
+    
+    ContextHandlerCollection contexts = new ContextHandlerCollection();
+    contexts.setHandlers(new Handler[] { helloContext, loginContext });
+       
+    server.setHandler(contexts);   
     server.start();
     server.join();
   }
